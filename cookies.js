caac2341a6b112e8d8c9146582a2e4853819e5df
Piotr Barcik <piotrbarcik83@gmail.com>
2017-08-20 19:00:05 +0200

    zmiana menu i napisu o ciasteczkach

--- cookies.js
+++ cookies.js
@@ -5,7 +5,11 @@
     if( !localStorage.getItem('theothers-cookie-info')){
         var cookieInfo = document.createElement('div');
         cookieInfo.setAttribute('id', 'cookieInfo');
-        cookieInfo.innerHTML = "<p>Ta strona przechowuje cookies.</p><span id='closeCookieInfo'>&times;</span>";
+        cookieInfo.innerHTML = "<p>Wykorzystujemy ciasteczka (ang. cookies) oraz podobne technologie" +
+            " w celu świadczenia usług, dostosowania serwisu do indywidualnych preferencji użytkowników oraz w celach" +
+            " statystycznych i reklamowych. Możesz zawsze wyłączyć ten mechanizm w ustawieniach przeglądarki. Korzystanie" +
+            " z naszego serwisu bez zmiany ustawień przeglądarki oznacza," +
+            " że cookies będą zapisane w pamięci urządzenia. </p><span id='closeCookieInfo'>&times;</span>";
         document.body.appendChild(cookieInfo);
         document.getElementById('closeCookieInfo').addEventListener('click', function(){
             document.body.removeChild(document.getElementById('cookieInfo'));
