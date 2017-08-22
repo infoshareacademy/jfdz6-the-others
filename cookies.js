b6f010cba40019672788404cba1587aad00262ee
Kaja <kajutella@gmail.com>
2017-08-22 21:13:05 +0200

    add cookies text change cookies style

--- cookies.js
+++ cookies.js
@@ -1,13 +1,13 @@
 /**
  * Created by marcinfortuna on 06.08.17.
  */
-(function(){
-    if( !localStorage.getItem('theothers-cookie-info')){
+(function () {
+    if (!localStorage.getItem('theothers-cookie-info')) {
         var cookieInfo = document.createElement('div');
         cookieInfo.setAttribute('id', 'cookieInfo');
-        cookieInfo.innerHTML = "<p>Ta strona przechowuje cookies.</p><span id='closeCookieInfo'>&times;</span>";
+        cookieInfo.innerHTML = "<h1>Ta strona przechowuje cookies.</h1><p>theothers.com korzysta z plików cookies. Pozostając na tej stronie, wyrażasz zgodę na korzystanie z tych plików.</p><span id='closeCookieInfo'>&times;</span>";
         document.body.appendChild(cookieInfo);
-        document.getElementById('closeCookieInfo').addEventListener('click', function(){
+        document.getElementById('closeCookieInfo').addEventListener('click', function () {
             document.body.removeChild(document.getElementById('cookieInfo'));
             localStorage.setItem('theothers-cookie-info', '1');
         });
