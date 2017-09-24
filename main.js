d8078d3b0697738a4bb8ecbe081ca71e0af78912
Norbert Brzeski <norbert-brzeski@wp.pl>
2017-09-24 12:41:18 +0200

    menu fix 1.3

--- main.js
+++ main.js
@@ -2,163 +2,44 @@
  * Created by marcin on 21.08.17.
  */
 
+// Menu sections hiding
+
 function main() {
-    $('#Contact').hide();
-    $('#AboutUs').hide();
-    $('#Info').hide();
-    $('#Timetable').hide();
-    $('#Navigate').hide();
+    var allIds = []
 
-    $('.fa-connectdevelop').parent().on('click', function () {
-            document.title = 'Komunikacja Miejska w Gdańsku';
-            $('#AboutUs').hide();
-            $('#Mainpage').show();
-            $('#Contact').hide();
-            $('#Info').hide();
-            $('#Timetable').hide();
-            $('#Navigate').hide();
-        }
-    );
+    function hideAll() {
+        $(allIds.join(',')).hide()
+    }
 
-    $('.fa-users').parent().on('click', function () {
-            document.title = 'O nas';
-            $('#AboutUs').show();
-            $('#Mainpage').hide();
-            $('#Contact').hide();
-            $('#Info').hide();
-            $('#Timetable').hide();
-            $('#Navigate').hide();
-        }
-    );
+    $('.ulmenu > li > a').each(function () {
+        allIds.push($(this).attr('href'))
 
-    $('.fa-envelope-o').parent().on('click', function () {
-            document.title = 'Kontakt';
-            $('#AboutUs').hide();
-            $('#Mainpage').hide();
-            $('#Contact').show();
-            $('#Info').hide();
-            $('#Timetable').hide();
-            $('#Navigate').hide();
-        }
-    );
+    }).on('click', function () {
+        setTimeout(function () {
+            $('html, body').scrollTop(0)
+        }, 0)
+    })
 
-    $('.fa-info').parent().on('click', function () {
-            document.title = 'Informacje o komunikacji';
-            $('#AboutUs').hide();
-            $('#Mainpage').hide();
-            $('#Contact').hide();
-            $('#Info').show();
-            $('#Timetable').hide();
-            $('#Navigate').hide();
-        }
-    );
+    $('.ulmenu a').on('click', function () {
+        hideAll()
+        $($(this).attr('href').split('/')[0]).show()
+        document.title = $(this).attr('data-title');
+    })
 
-    $('.fa-street-view').parent().on('click', function () {
-            document.title = 'Wyznacz trasę';
-            $('#AboutUs').hide();
-            $('#Mainpage').hide();
-            $('#Contact').hide();
-            $('#Info').hide();
-            $('#Timetable').hide();
-            $('#Navigate').show();
-        }
-    );
+    hideAll()
+    $('#Mainpage').show()
 
-    $('.fa-book').parent().on('click', function () {
-            document.title = 'Rozkład jazdy';
-            $('#AboutUs').hide();
-            $('#Mainpage').hide();
-            $('#Contact').hide();
-            $('#Info').hide();
-            $('#Timetable').show();
-            $('#Navigate').hide();
-        }
-    );
+// The End of Menu sections hiding
 
-    $('.fa-bus').parent().on('click', function () {
-            document.title = 'Rozkład jazdy';
-            $('#AboutUs').hide();
-            $('#Mainpage').hide();
-            $('#Contact').hide();
-            $('#Info').hide();
-            $('#Timetable').show();
-            $('#Navigate').hide();
-        }
-    );
+// Info sections hiding
 
-    $('.fa-subway').parent().on('click', function () {
-            document.title = 'Rozkład jazdy';
-            $('#AboutUs').hide();
-            $('#Mainpage').hide();
-            $('#Contact').hide();
-            $('#Info').hide();
-            $('#Timetable').show();
-            $('#Navigate').hide();
-        }
-    );
+    $('.info-section').hide();
+    $('.info-button').on('click', function () {
+        $(this).nextAll('.info-section:first').toggle();
+    });
 
-    $('.fa-train').parent().on('click', function () {
-            document.title = 'Rozkład jazdy';
-            $('#AboutUs').hide();
-            $('#Mainpage').hide();
-            $('#Contact').hide();
-            $('#Info').hide();
-            $('#Timetable').show();
-            $('#Navigate').hide();
-        }
-    );
+// The End of Info sections hiding
 
-    $('.fa-calendar').parent().on('click', function () {
-            document.title = 'Rozkład jazdy';
-            $('#AboutUs').hide();
-            $('#Mainpage').hide();
-            $('#Contact').hide();
-            $('#Info').hide();
-            $('#Timetable').show();
-            $('#Navigate').hide();
-        }
-    );
-
-    $('.skm-hist').hide();
-    $('.skm-tab').hide();
-    $('.pkm-hist').hide();
-    $('.pkm-tab').hide();
-    $('.skm-hist-button').on('click', function () {
-            $('.skm-hist').toggle();
-        }
-    );
-    $('.skm-tab-button').on('click', function () {
-            $('.skm-tab').toggle();
-        }
-    );
-    $('.pkm-hist-button').on('click', function () {
-            $('.pkm-hist').toggle();
-        }
-    );
-    $('.pkm-tab-button').on('click', function () {
-            $('.pkm-tab').toggle();
-        }
-    );
 }
 
 $(document).ready(main);
-
-
-// ZTM
-
-$('.ztmhsjq').hide();
-
-$(document).ready(function () {
-    $(".ztm-hist-button").click(function () {
-        $(".ztmhsjq").toggle();
-    });
-});
-
-
-$(document).ready(function () {
-    $(".ztm-tab-button").click(function () {
-        $(".ztmtbjq").toggle();
-    });
-});
-
-// ZTM
