bb4f68f7a7a021d83f9ca033d5de962c80271c53
Piotr Barcik <piotrbarcik83@gmail.com>
2017-10-15 13:56:07 +0200

    Add run one(blue) monster.

--- game.js
+++ game.js
@@ -43,7 +43,8 @@ function createTable(width, height) {
 
             var type = mapTable[j][i];
             if (objects[type]) {
-                $td.html('<img src="Assets/Photos/' + objects[type].img + '.png" style="width: 30px; height: 30px; background: ' + objects[type].bg + '" />').addClass(objects[type].class)
+                $td.html('<img src="Assets/Photos/' + objects[type].img + '.png" style="width: 30px; height: 30px; background: ' + objects[type].bg + '" />')
+                    .addClass(objects[type].class).addClass(objects[type].img)
             }
 
             $tr.append($td);
@@ -82,6 +83,27 @@ function moveDown(element) {
     return element.parent().next().find('td').eq(element.index());
 }
 
+var UP = 38, DOWN = 40, LEFT = 37, RIGHT = 39;
+
+function getNextPosition (element, way) {
+    var target = null;
+    if (way === 37) {
+        target = moveLeft(element);
+    } else if (way === 38) {
+        target = moveUp(element);
+    } else if (way === 39) {
+        target = moveRight(element);
+    } else {
+        target = moveDown(element);
+    }
+
+    if (!target.is('.wall')) {
+        return target;
+    }
+
+    return element;
+}
+
 function move(element, way, elClass) {
     var target = null;
     if (way === 37) {
@@ -106,4 +128,16 @@ $(document).keydown(function (e) {
         e.preventDefault();
         move($bus, e.keyCode, 'bus');
     }
-})
+});
+
+var monsterMoves = [UP, UP, UP, UP, UP, UP, DOWN, DOWN, DOWN, DOWN, DOWN, DOWN];
+var monsterMoveId = 0;
+
+var interval = setInterval(function() {
+    var monster = $('.obj0');
+
+    var $img = monster.removeClass('obj0').find('img');
+
+  getNextPosition(monster, monsterMoves[monsterMoveId]).append($img).addClass('obj0');
+  monsterMoveId = (monsterMoveId + 1) % monsterMoves.length
+}, 1000);
